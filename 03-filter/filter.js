4e92674af3ebc45546d2946148a0ab4c8c07e476
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-21 15:50:44 +0200

    exercise #3 done.

--- 03-filter/filter.js
+++ 03-filter/filter.js
@@ -1,7 +1,21 @@
 // create a randomNumbers array, containing 100 random numbers
+let randomNumbers = []
+
+for(let i= 0; i < 100; i++){
+    randomNumbers.push(Math.ceil(Math.random() * 200))
+}
+
+console.log( "\n Random Numbers : \n " + randomNumbers)
 
 // create a function isPrime(number)
 
+const isPrime = (number) =>{
+    for(let i = 2; i< number; i++ ){
+        if(number % i === 0) return false;
+    return number > 1
+    }
+}
+
 // create a primeNumbers array
-const primeNumbers = // filter randomNumbers with isPrime(number)
-console.log(primeNumbers)
+const primeNumbers = randomNumbers.filter(number => isPrime(number) ) // filter randomNumbers with isPrime(number)
+console.log("\n Prime Filtered Random Numbers : \n " + primeNumbers)
