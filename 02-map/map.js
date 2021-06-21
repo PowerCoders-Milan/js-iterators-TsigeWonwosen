991c82f85b31a871db09e29fd4a92e7d48f4a5e1
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-21 15:34:39 +0200

    exercise #2 done.

--- 02-map/map.js
+++ 02-map/map.js
@@ -1,6 +1,6 @@
 const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-const secretMessage = // use .map to get the first letter of every animal name in the arrray animals
+const secretMessage = animals.map(animal => animal[0])// use .map to get the first letter of every animal name in the arrray animals
 
 console.log(secretMessage.join(''));
 
@@ -8,6 +8,5 @@ console.log(secretMessage.join(''));
 const bigNumbers = [100, 200, 300, 400, 500];
 
 // Create the smallNumbers array below with map
-
-
+let smallNumbers = bigNumbers.map(bigNumber => bigNumber / 10);
 console.log(smallNumbers);
